a81b2757b06aec7c554c2440cf28f289158f0dae
Kenny Do <chinesedewey@gmail.com>
2016-03-06 19:23:56 -0800

    update how channels are configured

--- config.go
+++ config.go
@@ -9,23 +9,34 @@ import (
 
 // Config holds the configuration that Pino expects
 type Config struct {
-	IRC            IRCConfig         `yaml:"IRC"`
-	Slack          SlackConfig       `yaml:"Slack"`
-	ChannelMapping map[string]string `yaml:"ChannelMapping"`
+	IRC            IRCConfig                   `yaml:"IRC"`
+	Slack          SlackConfig                 `yaml:"Slack"`
+	ChannelMapping map[SlackChannel]IRCChannel `yaml:"ChannelMapping"`
 }
+
+// IRCChannel is the name of an IRC channel, like "#CAA"
+type IRCChannel string
+
+// IRCChannelKey is an optional password for an IRC channel
+type IRCChannelKey string
+
+// SlackChannel is the name of a Slack channel, like "#CAA-on-Slack"
+type SlackChannel string
 
 // IRCConfig define the IRC-specific config
 type IRCConfig struct {
-	Nickname string `yaml:"Nickname"`
-	Name     string `yaml:"Name"`
-	Server   string `yaml:"Server"`
-	IsSSL    bool   `yaml:"IsSSL"`
+	Nickname string                       `yaml:"Nickname"`
+	Name     string                       `yaml:"Name"`
+	Server   string                       `yaml:"Server"`
+	IsSSL    bool                         `yaml:"IsSSL"`
+	Channels map[IRCChannel]IRCChannelKey `yaml:"Channels"`
 }
 
 // SlackConfig defines the Slack-specific config
 type SlackConfig struct {
-	Owner string `yaml:"Owner"`
-	Token string `yaml:"Token"`
+	Owner    string                  `yaml:"Owner"`
+	Token    string                  `yaml:"Token"`
+	Channels map[SlackChannel]string `yaml:"Channels"`
 }
 
 // LoadConfig returns the Config parsed from the given config file path
